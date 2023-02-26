97b2fb00ae7eea2c9e026a974107157dab1e6d14
P.Kurakin <Kurakin93@gmail.com>
2023-02-26 17:35:26 +0300

    HW9 40 и 42 задачи

---
+++ WellDone(HW)/HW9.py
@@ -0,0 +1,18 @@
+import pandas as pa
+print("\n")
+file = pa.read_csv(r'173\california_housing_train.csv')
+print("---------------Выводим с 0 по 9 индексы таблицы (10 значений)-----------------\n")
+print(file.head(10))
+
+# Задача 40: Работать с файлом california_housing_train.csv, который находится в папке
+# sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
+print("\n")
+print("---------------Средняя стоиомсть дома с населением до 500------------------\n")
+print(file[file["population"] <= 500].median().medianHouseValue)
+
+
+# Задача 42: Узнать какая максимальная households в зоне минимального значения population
+print("\n")
+print("---------------Максимальная households в зоне минимального значения population------------------\n")
+print((file["households"].max()) & (file["population"].min()))
+print("\n")
