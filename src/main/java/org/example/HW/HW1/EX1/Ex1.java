322bfad7ca1274d12647fc96fc5438fbab7123b0
P.Kurakin <Kurakin93@gmail.com>
2023-03-08 22:43:37 +0300

    Ex1 - Well done. Final

--- src/main/java/org/example/HW/HW1/EX1/Ex1.java
+++ src/main/java/org/example/HW/HW1/EX1/Ex1.java
@@ -1,25 +1,44 @@
 package org.example.HW.HW1.EX1;
+/**
+Given an input string s, reverse the order of the words.
+        A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+        Return a string of the words in reverse order concatenated by a single space.
+        Note that s may contain leading or trailing spaces or multiple spaces between two words.
+        The returned string should only have a single space separating the words. Do not include any extra spaces.
 
-//Given an input string s, reverse the order of the words.
-//        A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
-//        Return a string of the words in reverse order concatenated by a single space.
-//        Note that s may contain leading or trailing spaces or multiple spaces between two words.
-//        The returned string should only have a single space separating the words. Do not include any extra spaces.
-//        Example 1:
-//        Input: s = "the sky is blue"
-//        Output: "blue is sky the"
+        Example 1:
+        Input: s = "the sky is blue"
+        Output: "blue is sky the"
 
+        Example 2
+        Input: s = "  hello world  "
+        Output: "world hello"
+        Explanation: Your reversed string should not contain leading or trailing spaces.
+
+        Example 3
+        Input: s = "a good   example"
+        Output: "example good a"
+        Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ **/
+import java.util.Scanner;
 public class Ex1 {
     public static void main(String[] args) {
-        String s = "the sky is blue";
-        String[] arr = s.split("\\b");
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Input: ");
+        String text = iScanner.nextLine();
+        iScanner.close();
+        System.out.println(reverse(text));
 
-        StringBuilder text_rev = new StringBuilder();
-        for (int i = arr.length-1; i >= 0; i--) {
-            text_rev.append(arr[i]);
-        }
-        String reversedString = text_rev.toString();
-        System.out.println(reversedString);
     }
-    
+
+
+    public static StringBuffer reverse(String text) {
+        String[] arr = text.split("\\s+");
+        StringBuffer strBuf = new StringBuffer();
+        for (int i = arr.length-1; i >= 0; i--) {
+            strBuf.append(" " + arr[i]);
+        }
+//        strBuf.reverse();
+        return strBuf;
+    }
 }
