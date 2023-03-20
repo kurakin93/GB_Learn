84f4829a2abf9eb11a6c8b5b65e0ba3f6ab64d73
P.Kurakin <Kurakin93@gmail.com>
2023-03-20 17:24:16 +0300

    Домашняя работа, семинар5, первое задание и второе задание обьединено

---
+++ Java/main/java/org/example/HW/HW5/Ex1.java
@@ -0,0 +1,41 @@
+package org.example.HW.HW5;
+
+import java.util.*;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        entryAmounts();
+    }
+
+    /**
+     * Подсчитать количество вхождения каждого слова
+     * Пример:
+     * Россия идет вперед всей планеты. Планета — это не Россия.
+     * toLoverCase().
+     */
+    static void entryAmounts() {
+        String[] symbols = {" ", ".", ",", "—", "-","!","?","«","»"};
+        String[] newResult = "Россия идет вперед всей планеты. Планета — это не Россия.".toLowerCase().split(Arrays.toString(symbols));
+        List<String> text = new ArrayList<>();
+        Map<String, Integer> elementText = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < newResult.length; i++) {
+            if (newResult[i].isEmpty() == false) {
+                text.add(newResult[i]);
+                elementText.putIfAbsent(newResult[i], count);
+            }
+        }
+        for (Map.Entry<String, Integer> elem : elementText.entrySet()) {
+            count=0;
+            for (String elem1 : text) {
+                if (elem.getKey() == elem1.intern()) {
+                    count ++;
+                    elementText.put(elem.getKey(), count);
+                }
+            }
+//            System.out.println("Слово: " + elem.getKey() + " Колличество вхождений: " + count); // из решения первого условия
+        }
+        System.out.println(elementText);
+    }
+}
+
